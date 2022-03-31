4af92f21142002e56b946ba9f3cf9f4f72f24ec0
innerstella <suji0509@naver.com>
2022-03-31 14:45:22 +0900

    hw

--- Session5_HW/project/project/urls.py
+++ Session5_HW/project/project/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('count/', views.count, name='count'),
+    path('result/', views.result, name='result'),
 ]
