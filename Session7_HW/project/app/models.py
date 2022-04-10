9ec30b764f05370c151cd153b02f4b77454e2cda
innerstella <suji0509@naver.com>
2022-04-10 23:51:49 +0900

    hw7

---
+++ Session7_HW/project/app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.timezone import now
+
+# Create your models here.
+
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    duedate = models.DateField(default=now)
+
+    def __str__(self):
+        return self.title
